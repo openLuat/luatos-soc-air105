0a0c34f92738bfa8d0ba90686dca1ce989c7e69c
Wendal Chen <wendal1985@gmail.com>
2022-03-13 00:40:47 +0800

    add: 添加函数luat_mcu_hz

--- application/src/luat_mcu_air105.c
+++ application/src/luat_mcu_air105.c
@@ -31,7 +31,7 @@ int luat_mcu_set_clk(size_t mhz) {
 }
 
 int luat_mcu_get_clk(void) {
-    return 0;
+    return 192;
 }
 
 static uint8_t unique_id[16] = {0};
@@ -43,4 +43,8 @@ const char* luat_mcu_unique_id(size_t* t) {
 
 long luat_mcu_ticks(void) {
     return GetSysTickMS();
+}
+
+uint32_t luat_mcu_hz(void) {
+    return configTICK_RATE_HZ;
 }
