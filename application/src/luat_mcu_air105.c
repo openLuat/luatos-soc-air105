b973c3453021c1f59b7803d7e6de54be460d2be3
alienwalker <alienwalker@sina.com>
2022-03-14 20:58:19 +0800

    add:mcu的tick相关c层接口，给lua用

--- application/src/luat_mcu_air105.c
+++ application/src/luat_mcu_air105.c
@@ -25,13 +25,13 @@
 #include "app_interface.h"
 
 #include "FreeRTOS.h"
-
+extern uint32_t SystemCoreClock;
 int luat_mcu_set_clk(size_t mhz) {
     return 0;
 }
 
 int luat_mcu_get_clk(void) {
-    return 0;
+    return SystemCoreClock/1000000;
 }
 
 static uint8_t unique_id[16] = {0};
@@ -43,4 +43,21 @@ const char* luat_mcu_unique_id(size_t* t) {
 
 long luat_mcu_ticks(void) {
     return GetSysTickMS();
+}
+
+uint32_t luat_mcu_hz(void) {
+    return 1000;
+}
+
+uint64_t luat_mcu_tick64(void) {
+	return GetSysTick();
+}
+
+int luat_mcu_us_period(void) {
+	return SYS_TIMER_1US;
+}
+
+void luat_mcu_delay_us(int delay)
+{
+	Task_DelayUS(delay);
 }
